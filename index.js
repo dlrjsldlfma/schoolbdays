0dde95eb8d59e4d086fd321ead265424f165cd54
dlrjsldlfma <72308121+dlrjsldlfma@users.noreply.github.com>
2022-08-10 10:29:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const countEl = document.getElementById("count");
+const countEl = document.getElementById('count');
 
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch("https://api.countapi.xyz/update/schoolbdays/849f344f-c5f6-467b-bbe2-07963d84d525/?amount=1")
-        .then(res => res.json())
-        .then(res => {countEl.innerHTML = res.value();});
+	fetch('https://api.countapi.xyz/update/schoolbdays/849f344f-c5f6-467b-bbe2-07963d84d525/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
 }
