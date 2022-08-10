9c3699c616ba2707f9ce510796a612a91f7bf3f8
dlrjsldlfma <72308121+dlrjsldlfma@users.noreply.github.com>
2022-08-10 10:14:59 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const countEl = document.getElementById("count");
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch("https://api.countapi.xyz/update/schoolbdays/849f344f-c5f6-467b-bbe2-07963d84d525/?amount=0")
+        .then(res => res.json())
+        .then(res => {countEl.innerHTML = res.value();});
+}
